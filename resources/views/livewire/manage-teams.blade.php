58e27361d2f040bfb9dbbe56896af054621ac126
vahnmarty <vahnmarty@gmail.com>
2023-03-04 08:37:29 +0800

    teams ui

--- resources/views/livewire/manage-teams.blade.php
+++ resources/views/livewire/manage-teams.blade.php
@@ -1,11 +1,12 @@
 <div>
-
+    @section('header')
     <header class="flex justify-between px-8 py-6 bg-white">
         <h1 class="text-4xl font-bold leading-7 text-emerald-900 sm:leading-9">Teams</h1>
         <div>
             
         </div>
     </header>
+    @endsection
     
     <div class="px-8 py-12 bg-gray-100">
         {{ $this->table }}
