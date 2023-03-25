020c34f99cbb80fe1fa0633b8dc9eb2c765c1e49
vahnmarty <vahnmarty@gmail.com>
2023-03-26 01:20:29 +0800

    login with google

--- app/Http/Controllers/SocialiteLoginController.php
+++ app/Http/Controllers/SocialiteLoginController.php
@@ -12,8 +12,22 @@ class SocialiteLoginController extends Controller
         return Socialite::driver($provider)->redirect();
     }
 
-    public function handleProviderCallback(Request $request)
+    public function handleProviderCallback(Request $request, $provider)
     {
+        $socialUser = Socialite::driver($provider)->user();
 
+        $user = User::firstOrCreate(
+            [
+                'provider_id' => $socialUser->getId(),
+            ],
+            [
+                'email' => $socialUser->getEmail(),
+                'name' => $socialUser->getName()
+            ]
+        );
+
+        auth()->login($user, true);
+
+        return redirect('dashboard');
     }
 }
