211e65e1c8cf53bbd04ce41e653e02e226817f9d
vahnmarty <vahnmarty@gmail.com>
2023-03-26 23:44:18 +0800

    removed fb sdk

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -24,8 +24,6 @@
         @livewireStyles
     </head>
     <body>
-        <div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v16.0&appId=924627508738067&autoLogAppEvents=1" nonce="Wz1yCz3V"></script>
         <div class="antialiased text-gray-600">
             {{ $slot }}
         </div>
