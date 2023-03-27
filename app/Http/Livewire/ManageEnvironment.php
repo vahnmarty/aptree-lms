2eb17878e446f72b8a4f8fcfdcbe335c79d3d9d4
vahnmarty <vahnmarty@gmail.com>
2023-03-27 15:30:07 +0800

    environment settings

--- app/Http/Livewire/ManageEnvironment.php
+++ app/Http/Livewire/ManageEnvironment.php
@@ -15,9 +15,11 @@ class ManageEnvironment extends Component implements HasForms
 {
     use InteractsWithForms;
 
+    public $APP_TIMEZONE;
     public $FACEBOOK_ENABLE, $FACEBOOK_CLIENT_ID, $FACEBOOK_CLIENT_SECRET;
     public $TWITTER_ENABLE, $TWITTER_CLIENT_ID, $TWITTER_CLIENT_SECRET;
     public $GOOGLE_ENABLE, $GOOGLE_CLIENT_ID, $GOOGLE_CLIENT_SECRET;
+    public $DB_CONNECTION;
     
     public function render()
     {
@@ -26,31 +28,71 @@ class ManageEnvironment extends Component implements HasForms
 
     public function mount()
     {
+        $this->appForm->fill([
+            'APP_TIMEZONE' => config('app.timezone'),
+        ]); 
+
+        $db_driver = config('database.default');
+
+        $this->databaseForm->fill([
+            'DB_CONNECTION' => $db_driver,
+            'DB_HOST' => config('database.connections.' . $db_driver . '.host'),
+            'DB_PORT' => config('database.connections.' . $db_driver . '.port'),
+            'DB_DATABASE' => config('database.connections.' . $db_driver . '.database'),
+            'DB_USERNAME' => config('database.connections.' . $db_driver . '.username'),
+            'DB_PASSWORD' => config('database.connections.' . $db_driver . '.password'),
+        ]);
+
         $this->facebookForm->fill([
-            'FACEBOOK_CLIENT_ID' => env('FACEBOOK_CLIENT_ID'),
-            'FACEBOOK_CLIENT_SECRET' => env('FACEBOOK_CLIENT_SECRET'),
+            'FACEBOOK_CLIENT_ID' => config('services.facebook.client_id'),
+            'FACEBOOK_CLIENT_SECRET' => config('services.facebook.client_secret'),
         ]);
 
         $this->twitterForm->fill([
-            'TWITTER_CLIENT_ID' => env('TWITTER_CLIENT_ID'),
-            'TWITTER_CLIENT_SECRET' => env('TWITTER_CLIENT_SECRET'),
+            'TWITTER_CLIENT_ID' => config('services.twitter.client_id'),
+            'TWITTER_CLIENT_SECRET' => config('services.twitter.client_secret'),
         ]);
 
         $this->googleForm->fill([
-            'GOOGLE_CLIENT_ID' => env('GOOGLE_CLIENT_ID'),
-            'GOOGLE_CLIENT_SECRET' => env('GOOGLE_CLIENT_SECRET'),
+            'GOOGLE_CLIENT_ID' => config('services.google.client_id'),
+            'GOOGLE_CLIENT_SECRET' => config('services.google.client_secret'),
         ]);
     }
 
     protected function getForms(): array 
     {
         return [
+            'appForm' => $this->makeForm()
+                ->schema($this->getAppFormSchema()),
+            'databaseForm' => $this->makeForm()
+                ->schema($this->getDatabaseFormSchema()),
             'facebookForm' => $this->makeForm()
                 ->schema($this->getFacebookFormSchema()),
             'twitterForm' => $this->makeForm()
                 ->schema($this->getTwitterFormSchema()),
             'googleForm' => $this->makeForm()
                 ->schema($this->getGoogleFormSchema()),
+        ];
+    }
+
+    public function getAppFormSchema()
+    {
+        return [
+            TextInput::make('APP_TIMEZONE')
+            ->label('APP_TIMEZONE')
+            ->hint('View the list of [timezones](https://www.php.net/manual/en/timezones.php)')
+        ];
+    }
+
+    public function getDatabaseFormSchema()
+    {
+        return [
+            TextInput::make('DB_CONNECTION')->label('DB_CONNECTION'),
+            TextInput::make('DB_HOST')->label('DB_HOST'),
+            TextInput::make('DB_PORT')->label('DB_PORT'),
+            TextInput::make('DB_DATABASE')->label('DB_DATABASE'),
+            TextInput::make('DB_USERNAME')->label('DB_USERNAME'),
+            TextInput::make('DB_PASSWORD')->label('DB_PASSWORD'),
         ];
     }
 
